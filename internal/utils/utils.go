9781138afc5772ded30ec5a379a48452840777fd
unknown <good@mail.ru>
2022-05-26 23:54:10 +0300

    Revert "Revert "Revert "Revert "inc 3 try 3""""
    This reverts commit 8f4d29beb1f8095bf88f91a9552fb47d596aa27e.

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -5,7 +5,10 @@ import (
 )
 
 func CheckIfStringIsNumber(v string) bool {
-	if _, err := strconv.Atoi(v); err == nil {
+	if _, err1 := strconv.Atoi(v); err1 == nil {
+		return true
+	}
+	if _, err := strconv.ParseFloat(v, 64); err == nil {
 		return true
 	}
 	return false
